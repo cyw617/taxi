00f8d9663ad6e6d28940b461fe6db4e940480b3b
byronyi <yibairen1994@gmail.com>
2014-02-17 21:27:41 +0800

    Delete irrelevant file.

--- gen/com/noideabox/helloworld/R.java
+++ gen/com/noideabox/helloworld/R.java
@@ -33,8 +33,7 @@ public final class R {
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int display_message=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050004;
