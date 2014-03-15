863418d01c274bc701c77cf123a3e221c3dfd0c5
CY Wong <cyw617@users.noreply.github.com>
2014-03-15 15:29:49 +0800

    Recovered to Original Customer

--- TaxiClient/Customer/src/comp3111h/anytaxi/customer/MainActivity.java
+++ TaxiClient/Customer/src/comp3111h/anytaxi/customer/MainActivity.java
@@ -1,64 +1,43 @@
 package comp3111h.anytaxi.customer;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.EditText;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "comp3111h.anytaxi.customer.MESSAGE";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		
+		EditText editText1 = (EditText) findViewById(R.id.edit_message);
+		EditText editText2 = (EditText) findViewById(R.id.edit_message2);
+		String message1 = editText1.getText().toString();
+		String message2 = editText2.getText().toString();
+		
+		String message = message1+" "+message2;
+		
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
 
 }
