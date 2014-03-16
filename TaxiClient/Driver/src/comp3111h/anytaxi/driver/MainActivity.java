ace283897733ac74c3cbf772d3d01f1b2602bd78
CY Wong <cyw617@users.noreply.github.com>
2014-03-17 01:59:09 +0800

    Initialize Driver

--- TaxiClient/Driver/src/comp3111h/anytaxi/driver/MainActivity.java
+++ TaxiClient/Driver/src/comp3111h/anytaxi/driver/MainActivity.java
@@ -1,27 +1,25 @@
 package comp3111h.anytaxi.driver;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+	}
+	
+	@Override
+	protected void onStart() {
+	    super.onStart();
+	}
+	
+	@Override
+	protected void onStop() {
+	    super.onStop();
 	}
 
 	@Override
@@ -43,22 +41,4 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-
 }
