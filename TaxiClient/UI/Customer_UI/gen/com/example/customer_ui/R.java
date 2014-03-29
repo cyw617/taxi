24005d9e80afdf537b4bd0ba0ddabd849e1bd116
Tony Mao <hongzimao@gmail.com>
2014-03-29 15:45:44 +0800

    use dialog

--- TaxiClient/UI/Customer_UI/gen/com/example/customer_ui/R.java
+++ TaxiClient/UI/Customer_UI/gen/com/example/customer_ui/R.java
@@ -41,7 +41,6 @@ public final class R {
         public static final int activity_login=0x7f030000;
         public static final int activity_request=0x7f030001;
         public static final int more_option=0x7f030002;
-        public static final int pop=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- TaxiClient/UI/Customer_UI/src/com/example/customer_ui/RequestActivity.java
+++ TaxiClient/UI/Customer_UI/src/com/example/customer_ui/RequestActivity.java
@@ -1,6 +1,8 @@
 package com.example.customer_ui;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -10,35 +12,41 @@ import android.widget.PopupWindow;
 
 public class RequestActivity extends Activity{
 	
-	PopupWindow popup;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_request);
 	}
 	
-	private void initPopWindow() {
-		 if (popup == null) {
-			 popup = new PopupWindow(getLayoutInflater().inflate(R.layout.pop, null),
-					 LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		 }
-		 if (popup.isShowing()) {
-			 popup.dismiss();
-		 }
-	}
+	private void showDialog(String message) {
+		  new AlertDialog.Builder(this)
+		      .setMessage(message)
+		      .setPositiveButton(android.R.string.ok,
+		          new DialogInterface.OnClickListener() {
+		    	  
+		    	    @Override
+		            public void onClick(DialogInterface dialog, int id) {
+		              dialog.dismiss();
+		            }
+		          })
+		          .setNegativeButton(android.R.string.cancel, 
+		        		  new DialogInterface.OnClickListener() {
+							
+							@Override
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.dismiss();
+								
+							}
+						}).show();
+		}
+	
 	public void MoreOption(View view) {
 		Intent intent = new Intent(this, MoreOption.class);
 		startActivity(intent);
 	}
 	
 	public void Request(View view){
-		initPopWindow();
-		popup.showAtLocation(view,Gravity.CENTER, 0, 0);
-	}
-	
-	public void Dismiss(View view){
-		popup.dismiss();
+		showDialog("test");
 	}
 	
 	public void logout(View view){
