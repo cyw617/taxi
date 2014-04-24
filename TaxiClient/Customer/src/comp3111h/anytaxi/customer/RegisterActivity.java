e61234273821a1cd32d6260c5ddca67233c53437
CY Wong <cyw617@users.noreply.github.com>
2014-04-24 20:22:25 +0800

    Implemented the logic of Agreement

--- TaxiClient/Customer/src/comp3111h/anytaxi/customer/RegisterActivity.java
+++ TaxiClient/Customer/src/comp3111h/anytaxi/customer/RegisterActivity.java
@@ -2,32 +2,53 @@ package comp3111h.anytaxi.customer;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
-public class RegisterActivity extends ActionBarActivity {
+public class RegisterActivity extends ActionBarActivity
+        implements Register_AgreementFragment.OnAgreementCheckedListener {
+    
+    public static String TAG = "RegisterActivity";
+    
+    private boolean agreement_isChecked = false;
+    
+    private Register_AgreementFragment agreementFragment = null;
+    private Register_FormFragment formFragment = null;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         
         // Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
+        // the register_fragment_container FrameLayout
         if (findViewById(R.id.register_fragment_container) != null) {
 
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
+            // If the activity is restored from a previous state,
+            // then nothing should be done or else
+            // there will be overlapping fragments.
             if (savedInstanceState == null) {
-                // Create a new Fragment to be placed in the activity layout
-                Register_AgreementFragment agreementFragment = new Register_AgreementFragment();
+                agreementFragment = new Register_AgreementFragment();
                 
-                // In case this activity was started with special instructions from an
-                // Intent, pass the Intent's extras to the fragment as arguments
-                agreementFragment.setArguments(getIntent().getExtras());
-                
-                // Add the fragment to the 'fragment_container' FrameLayout
                 getSupportFragmentManager().beginTransaction()
                         .add(R.id.register_fragment_container, agreementFragment).commit();
             }
+        }
+    }
+    
+    @Override
+    public void onAgreementChecked(boolean isChecked) {
+        Log.i(TAG, "Agreement is " + ((isChecked) ? "checked" : "unchecked") + ".");
+        
+        agreement_isChecked = isChecked;
+        
+        if (agreement_isChecked) {
+            if (formFragment == null)
+                formFragment = new Register_FormFragment();
+            
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.register_fragment_container, formFragment)
+                    .addToBackStack(null)
+                    .commit();
         }
     }
 }
