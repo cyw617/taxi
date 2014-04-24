45c566943cad52fc49790854bba80198b3139ece
CY Wong <cyw617@users.noreply.github.com>
2014-04-24 11:31:09 +0800

    Create two fragments for Agreement and Form

---
+++ TaxiClient/Customer/src/comp3111h/anytaxi/customer/Register_FormFragment.java
@@ -0,0 +1,15 @@
+package comp3111h.anytaxi.customer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Register_FormFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return null;
+    }
+}
