bee939785ea9b5b180168299a6712087324eca32
Byron Yi <yibairen1994@gmail.com>
2014-04-29 23:34:19 +0800

    for ci-test

--- TaxiClient/AnyTaxi_Customer_Test/src/comp3111h/anytaxi/customer/test/LoginActivityTest.java
+++ TaxiClient/AnyTaxi_Customer_Test/src/comp3111h/anytaxi/customer/test/LoginActivityTest.java
@@ -1,59 +1,93 @@
 package comp3111h.anytaxi.customer.test;
 
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.TouchUtils;
-import android.test.suitebuilder.annotation.SmallTest;
-import android.widget.Button;
-import android.widget.CheckBox;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.view.ContextThemeWrapper;
 import comp3111h.anytaxi.customer.LoginActivity;
+import comp3111h.anytaxi.customer.R;
+import comp3111h.anytaxi.customer.Utils;
 
-public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity>{
-    
-    private LoginActivity mActivity;
+public class LoginActivityTest extends ActivityUnitTestCase<LoginActivity> {
+	
+	private Context targetContext;
+	private Intent onActivityResultIntent;
+	
+	public LoginActivityTest() {
+		super(LoginActivity.class);
+	}
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		targetContext = getInstrumentation().getTargetContext();
+		
+		// set theme for AppCompat support
+		ContextThemeWrapper context = 
+				new ContextThemeWrapper(targetContext, R.style.AppTheme);
+		setActivityContext(context);
+		
+		Intent intent = new Intent(getInstrumentation().getTargetContext(),
+				LoginActivityTest.class);
+		
+		this.startActivity(intent, null, null);
+	}
+	
+	@MediumTest
+	public void testAccountPicker() {
+		this.getActivity().onStart();
+		assertEquals("wrong request for start account picker activity",
+				this.getStartedActivityRequest(), 
+				Utils.REQUEST_ACCOUNT_PICKER);
+	    
+	    final Intent launchIntent = this.getStartedActivityIntent();
+	    assertNotNull("Intent was null", launchIntent);
+	    assertEquals("intent was passed with wrong action",
+	    		launchIntent.getAction(),
+	    		"com.google.android.gms.common.account.CHOOSE_ACCOUNT");
+	}
+	
+	@MediumTest
+	public void testOnAcitivityResult() throws InterruptedException {
+		onActivityResultIntent = new Intent();
+		onActivityResultIntent.putExtra(AccountManager.KEY_ACCOUNT_NAME,
+				TestUtils.getCustomer().getEmail());
+		
+		this.getActivity().onActivityResult(Utils.REQUEST_ACCOUNT_PICKER,
+				Activity.RESULT_OK, onActivityResultIntent);
+		assertEquals("account name mismatch",
+				Utils.getPreference(getActivity(), Utils.PREFS_ACCOUNT_KEY, null),
+				TestUtils.getCustomer().getEmail());
 
-    public LoginActivityTest(){
-        super(LoginActivity.class);
-    }
-    
-    @Override
-    protected void setUp() throws Exception{
-        //this method is called every time before any test execution
-        super.setUp();
-        mActivity = (LoginActivity) getActivity();
-    }
-    
-    @Override
-    protected void tearDown() throws Exception{
-        //this method is called every time after any test execution
-        super.tearDown();
-    }
-    
-    @SmallTest
-    public void testView(){ // checks if the activity is created
-        assertNotNull(getActivity());
-    }
-    
-    /*
-    @SmallTest
-    public void testOnClick_In() {
-        TouchUtils.tapView(this, agreement_Chkbox);
-        TouchUtils.clickView(this, login_Btn_In);
-        
-        try {
-            Thread.sleep(3000);
-        }
-        catch (Exception e) {}
-        
-        assertTrue( googleApiClient.isConnected() );
-    }
-    
-    @SmallTest
-    public void testOnClick_Out() {
-        TouchUtils.clickView(this, login_Btn_Out);
-        
-        assertFalse( googleApiClient.isConnected() );
-    }*/
+	    assertTrue("Unable to connect Internet", Utils.isOnline(getActivity()));
+		Thread.sleep(5000);
+	    
+	    final Intent launchIntent = this.getStartedActivityIntent();
+	    assertNotNull("Intent was null", launchIntent);
+	    assertEquals(launchIntent.getComponent().getShortClassName(), ".RequestActivity");		
+	}
+	
+	@MediumTest
+	public void testRegisterActivityStart() throws InterruptedException {		
+		this.getActivity().onActivityResult(Utils.REQUEST_ACCOUNT_PICKER,
+				Activity.RESULT_OK, onActivityResultIntent);
+		assertNull("account name mismatch",
+				Utils.getPreference(getActivity(), Utils.PREFS_ACCOUNT_KEY, null));
+
+	    assertTrue("Unable to connect Internet", Utils.isOnline(getActivity()));
+		Thread.sleep(5000);
+	    
+	    final Intent launchIntent = this.getStartedActivityIntent();
+	    assertNotNull("Intent was null", launchIntent);
+	    assertEquals(launchIntent.getComponent().getShortClassName(), ".RegisterActivity");	
+	}
+	
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+		setActivity(null);
+	}
 }
