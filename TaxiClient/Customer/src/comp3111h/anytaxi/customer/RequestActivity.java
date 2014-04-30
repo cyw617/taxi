612c9fcd32de32e177c73a2c1332b76a4bfb7f2a
cochung <cochung1993@gmail.com>
2014-04-30 20:36:40 +0800

    the taxi share part has integrated into the orginal apps. u can click "more" to call it

--- TaxiClient/Customer/src/comp3111h/anytaxi/customer/RequestActivity.java
+++ TaxiClient/Customer/src/comp3111h/anytaxi/customer/RequestActivity.java
@@ -1,6 +1,5 @@
 package comp3111h.anytaxi.customer;
 
-
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
@@ -12,8 +11,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
@@ -23,70 +20,73 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 public class RequestActivity extends ActionBarActivity implements
-	GooglePlayServicesClient.ConnectionCallbacks,
-	GooglePlayServicesClient.OnConnectionFailedListener{
-	
-	//Temporary Variable
-	//Inspecting the connection status
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener
+{
+
+	// Temporary Variable
+	// Inspecting the connection status
 	static TextView mConnectionState;
 	static TextView mConnectionStatus;
 	static TextView mAddress;
 	static TextView mLatLng;
 	static ProgressBar mActivityIndicator;
-	
-	private Button moreButoon=(Button) findViewById(R.id.more);
-	
-	
+
+	private Button moreButoon = (Button) findViewById(R.id.more);
+
 	// Stores the current instantiation of the location client in this object
 	private LocationClient mLocationClient;
-	
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_request);
-		
-		//Identity user's identity, used to communicate with the server
+
+		// Identity user's identity, used to communicate with the server
 		/*
-		CustomerAccountInfo.credential = GoogleAccountCredential.usingAudience(this,
-				"server:client_id:" + CustomerAccountInfo.WEB_CLIENT_ID);
-				*/
-		
+		 * CustomerAccountInfo.credential =
+		 * GoogleAccountCredential.usingAudience(this, "server:client_id:" +
+		 * CustomerAccountInfo.WEB_CLIENT_ID);
+		 */
+
 		mLocationClient = new LocationClient(this, this, this);
 		mConnectionState = (TextView) findViewById(R.id.text_connection_state);
 		mConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
 		mAddress = (TextView) findViewById(R.id.address);
 		mLatLng = (TextView) findViewById(R.id.lat_lng);
 		mActivityIndicator = (ProgressBar) findViewById(R.id.address_progress);
-		
-		
-		LocationUtils.mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+		moreButoon.setOnClickListener(createButtonListener);
+
+		LocationUtils.mMap = ((MapFragment) getFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
 		LocationUtils.mMap.setMyLocationEnabled(true);
 	}
-	
+
 	@Override
-	public void onStart() {
+	public void onStart()
+	{
 		super.onStart();
 		/*
-		 * Connect the client. Don't re-start any requests here;
-		 * instead, wait for onResume()
+		 * Connect the client. Don't re-start any requests here; instead, wait
+		 * for onResume()
 		 */
 		mLocationClient.connect();
 	}
-	
+
 	/*
-	 * Called when the Activity is no longer visible at all.
-	 * Stop updates and disconnect.
+	 * Called when the Activity is no longer visible at all. Stop updates and
+	 * disconnect.
 	 */
 	@Override
-	public void onStop() {
+	public void onStop()
+	{
 		// After disconnect() is called, the client is considered "dead".
 		mLocationClient.disconnect();
 		super.onStop();
 	}
-	
-	
-	
+
 	private OnClickListener createButtonListener = new OnClickListener()
 	{
 		@Override
@@ -97,57 +97,61 @@ public class RequestActivity extends ActionBarActivity implements
 			startActivity(TestGroupMessageTable);
 		}
 	};
-	
-	
+
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent)
+	{
 
 		super.onActivityResult(requestCode, resultCode, intent);
 		// Choose what to do based on the request code
-		ConnectionUtils.requestCodeHandler(requestCode, resultCode, intent, this);
-	} 
-	
+		ConnectionUtils.requestCodeHandler(requestCode, resultCode, intent,
+				this);
+	}
+
 	/*
-	 * Called by Location Services when the request to connect the
-	 * client finishes successfully. At this point, you can
-	 * request the current location or start periodic updates
+	 * Called by Location Services when the request to connect the client
+	 * finishes successfully. At this point, you can request the current
+	 * location or start periodic updates
 	 */
 	@Override
-	public void onConnected(Bundle bundle) {
+	public void onConnected(Bundle bundle)
+	{
 		mConnectionStatus.setText(R.string.connected);
 
 		Location currentLoc = mLocationClient.getLastLocation();
-		while(currentLoc==null)
+		while (currentLoc == null)
 		{
 			currentLoc = mLocationClient.getLastLocation();
 		}
-		LatLng locationNew = new LatLng(currentLoc.getLatitude(),currentLoc.getLongitude());
-		CameraUpdate cameraup=CameraUpdateFactory.newLatLngZoom(locationNew,15);
+		LatLng locationNew = new LatLng(currentLoc.getLatitude(),
+				currentLoc.getLongitude());
+		CameraUpdate cameraup = CameraUpdateFactory.newLatLngZoom(locationNew,
+				15);
 		LocationUtils.mMap.animateCamera(cameraup);
-		
-		
-		LocationUtils.getAddress(this,currentLoc);
-		
+
+		LocationUtils.getAddress(this, currentLoc);
+
 	}
-	
+
 	@Override
-	public void onDisconnected() {
-	       Toast.makeText(this, "Disconnected. Please re-connect.",
-	       Toast.LENGTH_SHORT).show();
+	public void onDisconnected()
+	{
+		Toast.makeText(this, "Disconnected. Please re-connect.",
+				Toast.LENGTH_SHORT).show();
 	}
-	
+
 	/*
-	 * Called by Location Services if the attempt to
-	 * Location Services fails.
+	 * Called by Location Services if the attempt to Location Services fails.
 	 */
 	@Override
-	public void onConnectionFailed(ConnectionResult connectionResult) {
+	public void onConnectionFailed(ConnectionResult connectionResult)
+	{
 		/*
-		 * Google Play services can resolve some errors it detects.
-		 * If the error has a resolution, try sending an Intent to
-		 * start a Google Play services activity that can resolve
-		 * error.
+		 * Google Play services can resolve some errors it detects. If the error
+		 * has a resolution, try sending an Intent to start a Google Play
+		 * services activity that can resolve error.
 		 */
-		ConnectionUtils.connectionResultHandler(connectionResult,this);
+		ConnectionUtils.connectionResultHandler(connectionResult, this);
 	}
 }
