d23dbbc9432eba666e9eeb24c71fb77cfc44fa97
CY Wong <cyw617@users.noreply.github.com>
2014-05-04 18:28:59 +0800

    Incompleted test case

--- TaxiClient/AnyTaxi_Customer_Test/src/comp3111h/anytaxi/customer/test/RequestActivityTest.java
+++ TaxiClient/AnyTaxi_Customer_Test/src/comp3111h/anytaxi/customer/test/RequestActivityTest.java
@@ -3,21 +3,22 @@ package comp3111h.anytaxi.customer.test;
 import android.content.Context;
 import android.content.Intent;
 import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
 import android.view.ContextThemeWrapper;
+import android.widget.Button;
 
 import comp3111h.anytaxi.customer.R;
-import comp3111h.anytaxi.customer.RegisterActivity;
 import comp3111h.anytaxi.customer.RequestActivity;
 
 public class RequestActivityTest extends ActivityUnitTestCase<RequestActivity>{
 
-	private Context rContext; 
-	
-	public RequestActivityTest(Class<RequestActivity> activityClass) {
-		super(activityClass);
-	}
-	
-	@Override
+    private Context rContext; 
+    
+    public RequestActivityTest() {
+        super(RequestActivity.class);
+    }
+    
+    @Override
     protected void setUp() throws Exception {
         super.setUp();
         rContext = getInstrumentation().getTargetContext();
@@ -26,9 +27,22 @@ public class RequestActivityTest extends ActivityUnitTestCase<RequestActivity>{
         setActivityContext(new ContextThemeWrapper(rContext, R.style.AppTheme));
         
         // onCreate() will be called for the activity
-        Intent intent = new Intent(rContext, RegisterActivity.class);
+        Intent intent = new Intent(rContext, RequestActivity.class);
         startActivity(intent, null, null);
     }
-	
-	
+    
+    @SmallTest
+    public void testOnRequestListener() {
+        getActivity().onStart();
+        
+        Button requestButton = (Button) getActivity().findViewById(R.id.request_btn);
+        
+        assertNotNull("Request button is not found.", requestButton);
+        
+        requestButton.performClick();
+        
+        final Intent redirect = getStartedActivityIntent();
+        assertNotNull("No activity is started.", redirect);
+        assertEquals(redirect.getComponent().getShortClassName(), ".TrackingActivity");
+    }
 }
