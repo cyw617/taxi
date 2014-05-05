8fb72badb4ff9e3e79976a35966e1b24121509cc
CY Wong <cyw617@users.noreply.github.com>
2014-05-05 18:13:51 +0800

    [Customer] Fixed a bug in RequestActivity

--- TaxiClient/Customer/src/comp3111h/anytaxi/customer/RequestActivity.java
+++ TaxiClient/Customer/src/comp3111h/anytaxi/customer/RequestActivity.java
@@ -69,7 +69,7 @@ public class RequestActivity extends ActionBarActivity implements
         mAddress = (TextView) findViewById(R.id.address);
         mLatLng = (TextView) findViewById(R.id.lat_lng);
         mActivityIndicator = (ProgressBar) findViewById(R.id.address_progress);
-        myDestination = (String) ((TextView)findViewById(R.id.destination)).getText();
+        myDestination = ((TextView) findViewById(R.id.destination)).getText().toString();
 
         requestButton = (Button) findViewById(R.id.request_btn);
         requestButton.setOnClickListener(onRequestListener);
