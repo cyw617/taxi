86a1a592d1052ea6ebbad05c5ccac587256d6a6a
Tony Mao <hongzimao@gmail.com>
2014-05-05 21:30:21 +0800

    [Driver Test] update TraceActivityTest

--- TaxiClient/AnyTaxi_Driver_Test/src/comp3111h/anytaxi/driver/test/TraceActivityTest.java
+++ TaxiClient/AnyTaxi_Driver_Test/src/comp3111h/anytaxi/driver/test/TraceActivityTest.java
@@ -1,14 +1,17 @@
 package comp3111h.anytaxi.driver.test;
 
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.SmallTest;
+import comp3111h.anytaxi.driver.R;
 import comp3111h.anytaxi.driver.TraceActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.view.ContextThemeWrapper;
 
-public class TraceActivityTest extends ActivityInstrumentationTestCase2<TraceActivity>{
+public class TraceActivityTest extends ActivityUnitTestCase<TraceActivity>{
 	
-	private TraceActivity mActivity;
-	private static final double DELTA = 1e-2;
-	
+	private Context targetContext;
+    
 	public TraceActivityTest(){
 		super(TraceActivity.class);
 	}
@@ -17,18 +20,27 @@ public class TraceActivityTest extends ActivityInstrumentationTestCase2<TraceAct
 	protected void setUp() throws Exception{
 		//this method is called every time before any test execution
 		super.setUp();
-		mActivity = (TraceActivity) getActivity();
+		targetContext = getInstrumentation().getTargetContext();
+		
+		setActivityContext(new ContextThemeWrapper(targetContext, R.style.AppTheme));
+		
+		Intent intent = new Intent(getInstrumentation().getTargetContext(),
+				TraceActivity.class);
+		
+		this.startActivity(intent, null, null);
 	}
 	
 	@Override
 	protected void tearDown() throws Exception{
 		//this method is called every time after any test execution
-		
 		super.tearDown();
 	}
 	
 	@SmallTest
-	public void testView(){ // checks if the activity is created
+	public void testCustomerListActivityStart(){ // checks if the activity is created
+		
 		assertNotNull(getActivity());
 	}
+	
+	
 }
