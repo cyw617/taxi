4f016e8d5e560a633c26e715f170f1753c5f05d0
chpoon92 <chpoon92@gmail.com>
2014-05-06 16:44:05 +0800

    [Driver] Removed redundant codes

--- TaxiClient/Driver/src/comp3111h/anytaxi/driver/LoginActivity.java
+++ TaxiClient/Driver/src/comp3111h/anytaxi/driver/LoginActivity.java
@@ -3,10 +3,7 @@ package comp3111h.anytaxi.driver;
 import java.io.IOException;
 
 import android.accounts.AccountManager;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
@@ -39,11 +36,6 @@ public class LoginActivity extends ActionBarActivity {
 	}
 
 	@Override
-	public void onBackPressed() {
-		exit();
-	}
-
-	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		super.onActivityResult(requestCode, resultCode, intent);
 
@@ -73,39 +65,6 @@ public class LoginActivity extends ActionBarActivity {
 	public void startActivityViaIntent(Intent intent) {
 		startActivity(intent);
 	}
-
-	private void exit() {
-		showAlertDialog(this,
-				getString(R.string.quit_Message),
-				getString(R.string.quit_Positive),
-				getString(R.string.quit_Negative),
-				new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				moveTaskToBack(true);
-				finish();
-			}
-		}, null);
-	}
-
-	/**
-	 * @param context The current context of UI thread
-	 * @param msg Message to show in the body of dialog
-	 * @param posButtonText Text of positive button
-	 * @param negButtonText Text of negative button
-	 * @param posListener Click handler of positive button
-	 * @param negListener Click handler of negative button
-	 * 
-	 */
-	 public void showAlertDialog(Context context, String msg, String posButtonText,
-			 String negButtonText,  OnClickListener posListener, OnClickListener negListener) {
-		 new AlertDialog.Builder(context)
-		 .setMessage(msg)
-		 .setPositiveButton(posButtonText,
-				 posListener)
-				 .setNegativeButton(negButtonText, negListener)
-				 .show();
-	 }
 
 	 private class CheckLoginTask extends AsyncTask<Void, Void, Driver> {
 
