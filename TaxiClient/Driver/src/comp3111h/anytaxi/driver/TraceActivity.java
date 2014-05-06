659dce103b026426cf37257c1ff1e7a8367fa6df
ywangbc@gmail.com <9q9q99QQ>
2014-05-06 21:21:16 +0800

    Delete Redundancy in TraceActivity. Delete tracking activity

--- TaxiClient/Driver/src/comp3111h/anytaxi/driver/TraceActivity.java
+++ TaxiClient/Driver/src/comp3111h/anytaxi/driver/TraceActivity.java
@@ -53,11 +53,10 @@ public class TraceActivity extends FragmentActivity implements
     // Stores the current instantiation of the location client in this object
     private LocationClient mLocationClient;
     
-	private TextView mLatLng;
+
 	private TextView mAddress;
 	private ProgressBar mActivityIndicator;
-	private TextView mConnectionState;
-	private TextView mConnectionStatus;
+
     
 	//The lat and lng passed by CusTomerListActivity
 	private double latDouble;
@@ -100,12 +99,9 @@ public class TraceActivity extends FragmentActivity implements
 		setContentView(R.layout.activity_trace);
 		
 		// Get handles to the UI view objects
-        mLatLng = (TextView) findViewById(R.id.lat_lng);
         mAddress = (TextView) findViewById(R.id.address);
         mActivityIndicator = (ProgressBar) findViewById(R.id.address_progress);
-        mConnectionState = (TextView) findViewById(R.id.text_connection_state);
-        mConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
-        
+
         mMapFragment = SupportMapFragment.newInstance();
         FragmentTransaction fragmentTransaction =
                  getSupportFragmentManager().beginTransaction();
@@ -130,26 +126,7 @@ public class TraceActivity extends FragmentActivity implements
 		}
 		
 		
-		TextView textView = (TextView)findViewById(R.id.textView1);
-		textView.setText(latString+" "+lntString);
-		
-		
-        // Create a new global location parameters object
-        mLocationRequest = LocationRequest.create();
 
-        /*
-         * Set the update interval
-         */
-        mLocationRequest.setInterval(LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS);
-
-        // Use high accuracy
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        // Set the interval ceiling to one minute
-        mLocationRequest.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
-
-        // Note that location updates are off until the user turns them on
-        mUpdatesRequested = true;
 
         // Open Shared Preferences
         mPrefs = getSharedPreferences(LocationUtils.SHARED_PREFERENCES, Context.MODE_PRIVATE);
@@ -206,11 +183,6 @@ public class TraceActivity extends FragmentActivity implements
 
         super.onStart();
 
-        /*
-         * Connect the client. Don't re-start any requests here;
-         * instead, wait for onResume()
-         */
-        mLocationClient.connect();
         
 
     }
@@ -221,11 +193,6 @@ public class TraceActivity extends FragmentActivity implements
      */
     @Override
     public void onStop() {
-
-        // If the client is connected
-        if (mLocationClient.isConnected()) {
-            stopPeriodicUpdates();
-        }
 
         // After disconnect() is called, the client is considered "dead".
         mLocationClient.disconnect();
@@ -239,6 +206,9 @@ public class TraceActivity extends FragmentActivity implements
     @Override
     public void onPause() {
 
+    	
+        
+    	
         // Save the current setting for updates
         mEditor.putBoolean(LocationUtils.KEY_UPDATES_REQUESTED, mUpdatesRequested);
         mEditor.commit();
@@ -260,9 +230,6 @@ public class TraceActivity extends FragmentActivity implements
         if (servicesConnected()) {
             // Get the current location
             Location currentLocation = mLocationClient.getLastLocation();
-
-            // Display the current location in the UI
-            mLatLng.setText(LocationUtils.getLatLng(this, currentLocation));
             
             //Double lat=currentLocation.getLatitude();
             //Double lnt=currentLocation.getLongitude();
@@ -270,65 +237,8 @@ public class TraceActivity extends FragmentActivity implements
         }
     }
 
-    /**
-     * Invoked by the "Get Address" button.
-     * Get the address of the current location, using reverse geocoding. This only works if
-     * a geocoding service is available.
-     *
-     * @param v The view object associated with this method, in this case a Button.
-     */
-    // For Eclipse with ADT, suppress warnings about Geocoder.isPresent()
-    @SuppressLint("NewApi")
-    public void getAddress() {
 
-        // In Gingerbread and later, use Geocoder.isPresent() to see if a geocoder is available.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD && !Geocoder.isPresent()) {
-            // No geocoder is present. Issue an error message
-            Toast.makeText(this, R.string.no_geocoder_available, Toast.LENGTH_LONG).show();
-            return;
-        }
 
-        if (servicesConnected()) {
-
-            // Get the current location
-            Location currentLocation = mLocationClient.getLastLocation();
-
-            // Turn the indefinite activity indicator on
-            mActivityIndicator.setVisibility(View.VISIBLE);
-
-            // Start the background task
-            (new TraceActivity.GetAddressTask(this)).execute(currentLocation);
-        }
-    }
-
-    /**
-     * Invoked by the "Start Updates" button
-     * Sends a request to start location updates
-     *
-     * @param v The view object associated with this method, in this case a Button.
-     */
-    public void startUpdates(View v) {
-        mUpdatesRequested = true;
-
-        if (servicesConnected()) {
-            startPeriodicUpdates();
-        }
-    }
-
-    /**
-     * Invoked by the "Stop Updates" button
-     * Sends a request to remove location updates
-     * request them.
-     *
-     * @param v The view object associated with this method, in this case a Button.
-     */
-    public void stopUpdates(View v) {
-        mUpdatesRequested = false;
-
-        if (servicesConnected()) {
-            stopPeriodicUpdates();
-        }
-    }
     /*
     
     /*
@@ -338,6 +248,12 @@ public class TraceActivity extends FragmentActivity implements
     public void onResume() {
         super.onResume();
 
+        /*
+         * Connect the client. Don't re-start any requests here;
+         * instead, wait for onResume()
+         */
+        mLocationClient.connect();
+        
         // If the app already has a setting for getting location updates, get it
         if (mPrefs.contains(LocationUtils.KEY_UPDATES_REQUESTED)) {
             mUpdatesRequested = mPrefs.getBoolean(LocationUtils.KEY_UPDATES_REQUESTED, false);
@@ -365,11 +281,7 @@ public class TraceActivity extends FragmentActivity implements
      */
     @Override
     public void onConnected(Bundle bundle) {
-        mConnectionStatus.setText(R.string.connected);
 
-        
-        startPeriodicUpdates();
-        getAddress();
            
     }
 
@@ -380,7 +292,7 @@ public class TraceActivity extends FragmentActivity implements
      */
     @Override
     public void onDisconnected() {
-        mConnectionStatus.setText(R.string.disconnected);
+    	
     }
     
     
@@ -452,25 +364,6 @@ public class TraceActivity extends FragmentActivity implements
             return mDialog;
         }
     }
-	
-    /**
-     * In response to a request to start updates, send a request
-     * to Location Services
-     */
-    private void startPeriodicUpdates() {
-
-        mLocationClient.requestLocationUpdates(mLocationRequest, this);
-        mConnectionState.setText(R.string.location_requested);
-    }
-
-    /**
-     * In response to a request to stop updates, send a request to
-     * Location Services
-     */
-    private void stopPeriodicUpdates() {
-        mLocationClient.removeLocationUpdates(this);
-        mConnectionState.setText(R.string.location_updates_stopped);
-    }
     
     /**
      * Report location updates to the UI.
@@ -479,18 +372,6 @@ public class TraceActivity extends FragmentActivity implements
      */
     @Override
     public void onLocationChanged(Location location) {
-
-     
-
-    	
-        // Report to the UI that the location was updated
-        mConnectionStatus.setText(R.string.location_updated);
-
-        // In the UI, set the latitude and longitude to the value received
-        mLatLng.setText(LocationUtils.getLatLng(this, location));
-        
-        //Changes only need to be done in getLocation to cosntantly update the location info
-        //getLocation();
         
     }
     /*
@@ -516,9 +397,6 @@ public class TraceActivity extends FragmentActivity implements
                         // Log the result
                         Log.d(LocationUtils.APPTAG, getString(R.string.resolved));
 
-                        // Display the result
-                        mConnectionState.setText(R.string.connected);
-                        mConnectionStatus.setText(R.string.resolved);
                     break;
 
                     // If any other result was returned by Google Play services
@@ -526,9 +404,7 @@ public class TraceActivity extends FragmentActivity implements
                         // Log the result
                         Log.d(LocationUtils.APPTAG, getString(R.string.no_resolution));
 
-                        // Display the result
-                        mConnectionState.setText(R.string.disconnected);
-                        mConnectionStatus.setText(R.string.no_resolution);
+
 
                     break;
                 }
@@ -570,131 +446,6 @@ public class TraceActivity extends FragmentActivity implements
     }    
     
  
-    
-    /**
-     * An AsyncTask that calls getFromLocation() in the background.
-     * The class uses the following generic types:
-     * Location - A {@link android.location.Location} object containing the current location,
-     *            passed as the input parameter to doInBackground()
-     * Void     - indicates that progress units are not used by this subclass
-     * String   - An address passed to onPostExecute()
-     */
-    protected class GetAddressTask extends AsyncTask<Location, Void, String> {
-
-        // Store the context passed to the AsyncTask when the system instantiates it.
-        Context localContext;
-
-        // Constructor called by the system to instantiate the task
-        public GetAddressTask(Context context) {
-
-            // Required by the semantics of AsyncTask
-            super();
-
-            // Set a Context for the background task
-            localContext = context;
-        }
-
-        /**
-         * Get a geocoding service instance, pass latitude and longitude to it, format the returned
-         * address, and return the address to the UI thread.
-         */
-        @Override
-        protected String doInBackground(Location... params) {
-            /*
-             * Get a new geocoding service instance, set for localized addresses. This example uses
-             * android.location.Geocoder, but other geocoders that conform to address standards
-             * can also be used.
-             */
-            Geocoder geocoder = new Geocoder(localContext, Locale.getDefault());
-
-            // Get the current location from the input parameter list
-            Location location = params[0];
-
-            // Create a list to contain the result address
-            List <Address> addresses = null;
-
-            // Try to get an address for the current location. Catch IO or network problems.
-            try {
-
-                /*
-                 * Call the synchronous getFromLocation() method with the latitude and
-                 * longitude of the current location. Return at most 1 address.
-                 */
-                addresses = geocoder.getFromLocation(location.getLatitude(),
-                    location.getLongitude(), 1
-                );
-
-                // Catch network or other I/O problems.
-                } catch (IOException exception1) {
-
-                    // Log an error and return an error message
-                    Log.e(LocationUtils.APPTAG, getString(R.string.IO_Exception_getFromLocation));
-
-                    // print the stack trace
-                    exception1.printStackTrace();
-
-                    // Return an error message
-                    return (getString(R.string.IO_Exception_getFromLocation));
-
-                // Catch incorrect latitude or longitude values
-                } catch (IllegalArgumentException exception2) {
-
-                    // Construct a message containing the invalid arguments
-                    String errorString = getString(
-                            R.string.illegal_argument_exception,
-                            location.getLatitude(),
-                            location.getLongitude()
-                    );
-                    // Log the error and print the stack trace
-                    Log.e(LocationUtils.APPTAG, errorString);
-                    exception2.printStackTrace();
-
-                    //
-                    return errorString;
-                }
-                // If the reverse geocode returned an address
-                if (addresses != null && addresses.size() > 0) {
-
-                    // Get the first address
-                    Address address = addresses.get(0);
-
-                    // Format the first line of address
-                    String addressText = getString(R.string.address_output_string,
-
-                            // If there's a street address, add it
-                            address.getMaxAddressLineIndex() > 0 ?
-                                    address.getAddressLine(0) : "",
-
-                            // Locality is usually a city
-                            address.getLocality(),
-
-                            // The country of the address
-                            address.getCountryName()
-                    );
-
-                    // Return the text
-                    return addressText;
-
-                // If there aren't any addresses, post a message
-                } else {
-                  return getString(R.string.no_address_found);
-                }
-        }
-
-        /**
-         * A method that's called once doInBackground() completes. Set the text of the
-         * UI element that displays the address. This method runs on the UI thread.
-         */
-        @Override
-        protected void onPostExecute(String address) {
-
-            // Turn off the progress bar
-            mActivityIndicator.setVisibility(View.GONE);
-
-            // Set the address in the UI
-            mAddress.setText(address);
-        }
-    }
 
     
     /**
